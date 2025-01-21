69d4ab2fbc14771f525bf82c9e901cd70e719a83
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2025-01-21 05:50:45 +0100

    Improve dashboard page, adding links to reports

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,3 +1,15 @@
 // Import all of Bootstrap's JS
 // import * as bootstrap from 'bootstrap'
 import './dashboard.js'
+
+const highlightClassname = 'table-active'
+const hash = window.location.hash.substring(1) // Remove the '#' symbol
+if (hash) {
+  const targetElement = document.getElementById(hash)
+  if (targetElement && targetElement.nodeName == 'TR') {
+    targetElement.classList.add(highlightClassname)
+    setTimeout(() => {
+      targetElement.classList.remove(highlightClassname)
+    }, 2000)
+  }
+}
